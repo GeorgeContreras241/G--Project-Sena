644927e52d66b78370b7eae2f8182591f9fdfb0b
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-03-26 17:50:50 -0500

    feat(navigate): add new buttom

---
+++ components/ui/exit.tsx
@@ -0,0 +1,12 @@
+import { Arrow } from "./icons/Arrow";
+
+export const Exit = () => {
+    return (
+        <div>
+            <button className="w-20 h-20 rotate-180">
+                <Arrow />
+            </button>
+        </div>
+    )
+}
+
