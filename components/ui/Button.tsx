b89a6232a40951392a8537d04695f5e4f08421ba
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-04-06 11:34:19 -0500

    style(button):  modify tailwind css className

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,7 +2,8 @@ import { ReactNode } from "react"
 
 export const Button = ({children, text}: { children: ReactNode, text: string }) => {
     return (
-        <button className="flex gap-2 items-center bg-primary text-white px-4 py-2 rounded-md border border-border cursor-pointer">
+        <button className="flex gap-2 items-center bg-primary text-white px-4 py-2 rounded-md border border-border 
+        cursor-pointer hover:bg-bg-elevated">
             {children}
             {text}
         </button>
